52606d5a561e60b1bd68ee0984b7db4c432ca416
Oct1 <591102252@qq.com>
2018-09-25 22:08:34 +0800

    h5c3 v1.0

---
+++ h5c3/js/index.js
@@ -0,0 +1,28 @@
+$(function () {
+    $('.container').fullpage({
+        sectionsColor: ["#fadd67", "#84a2d4", "#ef674d", "#ffeedd", "#d04759", "#84d9ed", "#8ac060"],
+        verticalCentered: false,
+        navigation: true,
+        afterLoad: function (link, index) {
+            $('.section').eq(index - 1).addClass('now');
+        },
+        onLeave:function (index,nextIndex,direction) {
+            if(index  ==  2 && nextIndex == 3){
+                $('.section').eq(index-1).addClass('leaved');
+            }else if(index  ==  3 && nextIndex == 4){
+                $('.section').eq(index-1).addClass('leaved');
+            }
+        },
+        afterRender:function () {
+            $('.more').on('click',function () {
+                $.fn.fullpage.moveSectionDown();
+            });
+            $('.screen04 .cart').on('transitionend',function () {
+                $('.screen04 .address').show().find('img:last').fadeIn(1000);
+                $('.screen04 .text').addClass('show');
+            });
+        },
+        scrollingSpeed:1000
+    })
+
+});
